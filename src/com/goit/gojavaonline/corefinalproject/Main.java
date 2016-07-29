025085ead08f34825c61477b47c44fa77d24e73f
danil <ostapchenko.danil@gmail.com>
2016-07-29 12:36:33 +0300

    created final project

--- src/com/goit/gojavaonline/corefinalproject/Main.java
+++ src/com/goit/gojavaonline/corefinalproject/Main.java
@@ -3,6 +3,6 @@ package com.goit.gojavaonline.corefinalproject;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("asdasd");
     }
 }
