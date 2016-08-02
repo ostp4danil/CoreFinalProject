d2b042b2084f5a602e35a59176f68ecf0825a9fa
Vania <whateva>
2016-08-02 05:13:44 +0300

    new branch created/Added AnagrammSearcher class

--- src/com/goit/gojavaonline/corefinalproject/Main.java
+++ src/com/goit/gojavaonline/corefinalproject/Main.java
@@ -1,8 +1,12 @@
 package com.goit.gojavaonline.corefinalproject;
 
+import com.goit.gojavaonline.corefinalproject.anagramm.AnagramSearcher;
+import com.goit.gojavaonline.corefinalproject.anagramm.IAnagramSearcher;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("asdasd");
+        IAnagramSearcher searcher = new AnagramSearcher();
+        System.out.println(searcher.isAnagram("london, don don9  ","donlon don don9"));
     }
 }
